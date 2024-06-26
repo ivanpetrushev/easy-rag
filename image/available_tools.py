6d79cc9dca5905aed4ac8e2c935ba5aad61317b2
Ivan Petrushev <ivanatora@gmail.com>
2024-06-26 20:31:32 +0300

    Tools: prompts, actions, test data, chat history

---
+++ image/available_tools.py
@@ -0,0 +1,75 @@
+import os
+import json
+import requests
+from langchain_core.tools import tool
+
+contact_book = {}
+
+@tool
+def add_contact(contact_name: str, contact_email: str) -> str:
+    """Add a contact to the current user contact book. Contact book is a dictionary that associates people names with their email addresses.
+    The tool should be used only once per contact and only if it is new. If the contact already exists, the tool should not be used.
+    """
+    contact_book[contact_name] = contact_email
+    result = f"Contact {contact_name} added with email {contact_email}"
+    print(f"Function: add_contact({contact_name}, {contact_email}) = {result}")
+    return result
+
+@tool
+def send_email(recipient_address: str, email_subject: str, email_body: str) -> str:
+    "Send an email to a recipient with a known e-mail address. This tool should be used only once per email."
+    result = f"Email sent to {recipient_address}\n Subject: {email_subject}\n Body: {email_body}"
+    print(f"Function: send_email({recipient_address}, {email_subject}, {email_body}) = {result}")
+    return result
+
+
+@tool
+def search_email_address_by_name(name: str) -> str:
+    """Search for an email address in current user contact book. Use this, if you need the email address of a person and you don't know it. 
+    If the email address is not found, the function returns 'Email address not found'.
+    In this case it is not useful to invoke the same function with the same name again.
+    """
+    result = contact_book.get(name, "Email address not found")
+    print(f"Function: search_email_address_by_name({name}) = {result}")
+    return result
+
+
+@tool
+def create_jira_ticket(ticket_summary: str, ticket_description: str, assignee_name: str) -> str:
+    """
+    Create a Jira ticket with a summary, description, and assignee. 
+    Jira tickets are instructions for tasks given to a specific team member.
+    Target team member must be specified with his/her name.
+    Don't create multiple Jira tickets for the same task.
+    """
+    result = f"Jira ticket created with summary {ticket_summary}, description {ticket_description}, and assignee {assignee_name}"
+    print(f"Function: create_jira_ticket({ticket_summary}, {ticket_description}, {assignee_name}) = {result}")
+    return result
+
+@tool
+def send_message_to_google_chat_workspace(workspace_name: str, message: str):
+    """
+    Send a message to a Google Chat workspace. Don't use this to send repetetive messages.
+    """
+    print(f"Function: send_message_to_google_chat_workspace({workspace_name}, {message})")
+    workspace_webhooks = {
+        "test_bot_space": os.environ.get("GCHAT_TEST_BOT_SPACE_WEBHOOk"),
+    }
+    print(f"workspace_webhooks: {workspace_webhooks}")
+    if workspace_name not in workspace_webhooks:
+        return f"Webhook for workspace not found: {workspace_name}"
+    
+    headers = {
+        "Content-Type": "application/json; charset=UTF-8",
+    }
+    data = {
+        "text": message,
+    }
+    response = requests.post(workspace_webhooks[workspace_name], headers=headers, data=json.dumps(data))
+    if response.status_code != 200:
+        return f"Failed to send message to Google Chat workspace {workspace_name}: {response.text}"
+    
+    result = f"Message sent to Google Chat workspace {workspace_name}: {message}"
+    
+    return result
+
--- image/tool-usage.py
+++ image/tool-usage.py
@@ -1,14 +1,13 @@
-from langchain_core.tools import tool
-import json
 from langchain import hub
 from langchain.globals import set_debug, set_verbose
 from langchain.agents import AgentExecutor, create_tool_calling_agent
 from langchain_aws import ChatBedrock
-from langchain_core.prompts.chat import ChatPromptTemplate, PromptTemplate
+from langchain_core.prompts.chat import ChatPromptTemplate, PromptTemplate, MessagesPlaceholder
 from langchain_core.runnables import RunnablePassthrough
-
+from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
+from available_tools import add_contact, send_email, search_email_address_by_name, create_jira_ticket, send_message_to_google_chat_workspace 
 # set_debug(True)
-set_verbose(True)
+# set_verbose(True)
 
 # from langchain.agents import AgentExecutor, create_tool_calling_agent
 
@@ -16,125 +15,86 @@ set_verbose(True)
 # prompt = hub.pull("hwchase17/openai-tools-agent")
 # prompt.pretty_print()
 
-prompt = ChatPromptTemplate.from_template(
-    """
-<system-message>
-You are a helpful assistant. 
-If you are unable to produce answer, just say so.
-</system-message>
-<intermediate-steps>
-{intermediate_steps}
-</intermediate-steps>
-<human-message>
-{input}
-</human-message>
-<agent-scratchpad>
-{agent_scratchpad}
-</agent-scratchpad>
-"""
-)
-
 # prompt = ChatPromptTemplate.from_template(
 #     """
-# You are a helpful assistant, but you are bad at math.
-# Answer user questions using the tools provided below
-
+# <system-message>
+# You are a helpful assistant. 
+# If you are unable to produce answer, just say so.
+# Do not make guesses about emails or user IDs. If unsure, use the appropriate tool to find the information. If still unsure, ask the user.
+# Do not use tools unless necessary. Use a tool multiple times one after another only if needed.
+# If uncertain about the user's context, ask for clarification.
+# If asked to take action, perform action only once unless specifically asked to repeat.
+# Listen and act to human input only.
+# </system-message>
+# <chat-history>
+# {chat_history}
+# </chat-history>
+# <intermediate-steps>
+# {intermediate_steps}
+# </intermediate-steps>
+# <human>
 # {input}
-
-# Scratcpad: {agent_scratchpad}
-
-
+# </human>
+# <agent-scratchpad>
+# {agent_scratchpad}
+# </agent-scratchpad>
 # """
 # )
 
+prompt = ChatPromptTemplate.from_template(
+    """
+# Messages:
 
-@tool
-def multiply(first_int: int, second_int: int) -> int:
-    """Multiply two integers together."""
-    result = first_int * second_int
-    print(f"Function: multiply({first_int}, {second_int}) = {result}")
-    return result
+{messages}
 
+# Intermediate steps:
 
-@tool
-def add(first_int: int, second_int: int) -> int:
-    "Add two integers."
-    result = first_int + second_int
-    print(f"Function: add({first_int}, {second_int}) = {result}")
-    return result
+{intermediate_steps}
 
+# Agent scratchpad:
 
-@tool
-def exponentiate(base: int, exponent: int) -> int:
-    "Exponentiate the base to the exponent power."
-    result = base**exponent
-    print(f"Function: exponentiate({base}, {exponent}) = {result}")
-    return result
+{agent_scratchpad}
 
+# Human input:
+{input}
+"""
+)
 
-@tool
-def hyperspace(from_source: int, to_destination: int) -> int:
-    "Travel through hyperspace from one point to another."
-    result = from_source + to_destination
-    print(f"Function: hyperspace({from_source}, {to_destination}) = {result}")
-    return result
-
+# prompt = ChatPromptTemplate.from_messages([
+#     MessagesPlaceholder(variable_name="messages"),
+#     MessagesPlaceholder(variable_name="intermediate_steps"),
+#     MessagesPlaceholder(variable_name="agent_scratchpad")
+# ])
+messages = [
+    ("system", """
+You are a helpful assistant. 
+If you are unable to produce answer, just say so.
+Do not make guesses about emails or user IDs. If unsure, use the appropriate tool to find the information. If still unsure, ask the user.
+Do not use tools unless necessary. Use a tool multiple times one after another only if needed.
+If uncertain about the user's context, ask for clarification.
+If asked to take action, perform action only once unless specifically asked to repeat.
+Listen and act to human input only.
+Don't re-execute tasks given in the chat history, only from the current conversation.
+Explain your chain of thoughts.
+"""),
+]
 # communication tools
-contact_book = {}
-
-@tool
-def add_contact(contact_name: str, contact_email: str) -> str:
-    """Add a contact to the current user contact book. Contact book is a dictionary that associates people names with their email addresses.
-    The function stores the email address associated with the provided name in a dictionary called `contact_book`
-    """
-    contact_book[contact_name] = contact_email
-    result = f"Contact {contact_name} added with email {contact_email}"
-    print(f"Function: add_contact({contact_name}, {contact_email}) = {result}")
-    return result
-
-@tool
-def send_email(recipient_address: str, email_subject: str, email_body: str) -> str:
-    "Send an email to a recipient with a known e-mail address."
-    result = f"Email sent to {recipient_address} with subject {email_subject} and body {email_body}"
-    print(f"Function: send_email({recipient_address}, {email_subject}, {email_body}) = {result}")
-    return result
 
 
-@tool
-def search_email_address_by_name(name: str) -> str:
-    """Search for an email address by name. Use this, if you don't know the email address of a person. 
-    If the email address is not found, the function returns 'Email address not found'.
-    In this case it is not useful to invoke the same function with the same name again.
-    """
-    result = contact_book.get(name, "Email address not found")
-    print(f"Function: search_email_address_by_name({name}) = {result}")
-    return result
+chat_history = []
 
-
-@tool
-def create_jira_ticket(ticket_summary: str, ticket_description: str, assignee_name: str) -> str:
-    """
-    Create a Jira ticket with a summary, description, and assignee. 
-    Jira tickets are instructions for tasks given to a specific team member.
-    Target team member must be specified with his/her name.
-    """
-    result = f"Jira ticket created with summary {ticket_summary}, description {ticket_description}, and assignee {assignee_name}"
-    print(f"Function: create_jira_ticket({ticket_summary}, {ticket_description}, {assignee_name}) = {result}")
-    return result
-
-# math tools
 # tools = [multiply, add, exponentiate, hyperspace]
 
 
 # communication tools
-tools = [send_email, create_jira_ticket, search_email_address_by_name, add_contact]
+tools = [send_email, create_jira_ticket, search_email_address_by_name, add_contact, send_message_to_google_chat_workspace]
 
 model_id = "anthropic.claude-3-haiku-20240307-v1:0"
 model_kwargs = {
     "max_tokens": 1500,
     "anthropic_version": "bedrock-2023-05-31",
     "stop_sequences": ["User:"],
-    "temperature": 1,
+    "temperature": 0.5,
     "top_k": 250,
     "top_p": 0.999
 }
@@ -143,24 +103,8 @@ llm = ChatBedrock(
     model_id=model_id,
     model_kwargs=model_kwargs)
 
-llm_with_tools = llm.bind_tools(tools)
+# llm_with_tools = llm.bind_tools(tools)
 
-# tools_input = []
-# for tool in tools:
-#     tools_input.append({
-#         "name": tool.name,
-#         "description": tool.description,
-#         "input_schema": tool.args_schema.schema_json()
-#     })
-# tools_input = json.dumps(tools_input, indent=4)
-
-# chain = {
-#     "input": RunnablePassthrough(),
-# } | prompt | llm
-# input = "Take 3 to the fifth power as a source point, multiply 5 by two as a destination point, then travel through hyperspace from the source to the destination."
-# llm_with_tools.invoke(input)
-# chain.invoke(input + "\nAvailable tools: " + tools_input)
-# Construct the tool calling agent
 agent = create_tool_calling_agent(llm, tools, prompt)
 
 # # Create an agent executor by passing in the agent and tools
@@ -170,8 +114,14 @@ while True:
     input_str = input("\n\n\nEnter your input: ")
     if input_str == "exit":
         break
-    agent_executor.invoke(
+    
+    response = agent_executor.invoke(
         {
-            "input": input_str
+            "input": input_str,
+            # "chat_history": chat_history,
+            "messages": messages,
         }
     )
+    print(f"Response: {response['output']}")
+    messages.append(("human", input_str))
+    messages.append(("assistant", response["output"]))
